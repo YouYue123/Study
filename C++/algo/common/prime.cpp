b70e4c4e0894b925bcade396098e4bb9733b1edf
YouYue123 <youy0004123@gmail.com>
2025-06-17 22:46:03 +0800

    add sieves

---
+++ C++/algo/common/prime.cpp
@@ -0,0 +1,13 @@
+const int MAX_N = 1e5;
+bool is_composite[MAX_N + 1]; // is_composite[i] = true if i is composite, false if i is prime
+
+void sieve_primes() {
+    is_composite[1] = true; // 1 is not a prime number
+    for (int p = 2; p * p <= MAX_N; ++p) {
+        if (!is_composite[p]) { // If p is prime
+            for (int multiple = p * p; multiple <= MAX_N; multiple += p) {
+                is_composite[multiple] = true; // Mark multiples of p as composite
+            }
+        }
+    }
+}
