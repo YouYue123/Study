3893dd42e42502b7d449d8fb29f7892cceed0364
YouYue123 <youy0004123@gmail.com>
2025-06-23 10:53:02 +0800

    update

--- C++/algo/codeforce/practice/2117_C.cpp
+++ C++/algo/codeforce/practice/2117_C.cpp
@@ -1,7 +1,5 @@
 #include <bits/stdc++.h>
-#include <iostream>
 using namespace std;
-
 int main()
 {
     int t;
@@ -9,11 +7,26 @@ int main()
     while (t--)
     {
         int n;
-        cin >> n;
+        cin >> n; 
         vector<int> a(n);
+        unordered_map<int, int> dict;
         for (int i = 0; i < n; i++)
         {
             cin >> a[i];
+            dict[a[i]] += 1;
         }
+        int ans = 1;
+        unordered_set<int> prev;
+        prev.insert(a[0]);
+        unordered_set<int> cur;
+        for(int i = 1; i < n; i ++) {
+            if(prev.contains(a[i])) prev.erase(a[i]);
+            cur.insert(a[i]);
+            if(prev.empty()) {
+                ans += 1;
+                swap(prev, cur);
+            }
+        }
+        cout << ans << endl;
     }
 }
