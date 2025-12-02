b6b57fbc14d6b9a4ec0af09a337cc7b3f7cb8e62
You Yue <yue.you@grabtaxi.com>
2025-12-02 13:15:51 +0800

    2_1

---
+++ C++/algo/adventofcode/2025/2/2_1.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    string line;
+    ull ans = 0;
+    while(getline(cin, line)) {
+       stringstream ss(line);
+       // "11-22,95-115
+       while(getline(ss, line, ',')) {
+            stringstream ss2(line);
+            string start, end;
+            while(getline(ss2, start, '-')) {
+                getline(ss2, end, '-');
+                ull start_num = stoull(start);
+                ull end_num = stoull(end);
+                // Digit dp to find digits repeats twice
+                // cout << "start: " << start << " end: " << end << endl;
+                for(ull i = start_num; i <= end_num; i++) {
+                    string num_str = to_string(i);
+                    int half = num_str.size() / 2;
+                    if(num_str.substr(0, half) == num_str.substr(half)) {
+                        cout << "invalid: " << num_str << endl;
+                        ans += i;
+                    }
+                }
+            }
+       }
+    }
+    cout << ans << endl;
+}
