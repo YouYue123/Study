55d9de065b4d55a561a8afd04772dd8b2e543874
YouYue123 <youy0004123@gmail.com>
2025-12-06 07:55:20 +0800

    update

--- C++/algo/adventofcode/2025/5/5_1.cpp
+++ C++/algo/adventofcode/2025/5/5_1.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 using ll = long long;
 using ull = unsigned long long;
-using Range = pair<ull, ull>;
+using pii = pair<ull, ull>;
 int main() {
+    auto start = chrono::high_resolution_clock::now();
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
     string line;
-    vector<Range> fresh_ranges;
+    vector<pii> fresh_ranges;
     vector<ull> available_ids;
     bool reading_ranges = true;
     while (getline(cin, line)) {
@@ -16,16 +16,13 @@ int main() {
             reading_ranges = false;
             continue;
         }
-
         if (reading_ranges) {
             size_t dash_pos = line.find('-');
             if (dash_pos != string::npos) {
                 string start_str = line.substr(0, dash_pos);
                 string end_str = line.substr(dash_pos + 1);
-
                 ull start_num = stoull(start_str);
                 ull end_num = stoull(end_str);
-
                 fresh_ranges.push_back({start_num, end_num});
             }
         } else {
@@ -33,24 +30,20 @@ int main() {
             available_ids.push_back(id);
         }
     }
-
     ull ans = 0;
-
     for (ull id : available_ids) {
         bool is_fresh = false;
-
         for (const auto& range : fresh_ranges) {
             if (id >= range.first && id <= range.second) {
                 is_fresh = true;
                 break;
             }
         }
-
-        if (is_fresh) {
-            ans++;
-        }
+        if (is_fresh) ans++;
     }
-
-
     cout << "ans:" << ans << endl;
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::nanoseconds>(end - start);
+    cout << "ans:" << ans << endl;
+    cout << "runtime: " << duration.count() << " nanoseconds" << endl;
 }
--- C++/algo/adventofcode/2025/5/5_2.cpp
+++ C++/algo/adventofcode/2025/5/5_2.cpp
@@ -2,16 +2,15 @@
 using namespace std;
 using ll = long long;
 using ull = unsigned long long;
-using Range = pair<ull, ull>;
+using pii = pair<ull, ull>;
 int main() {
+    auto start = chrono::high_resolution_clock::now();
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
     string line;
-    vector<Range> ranges;
+    vector<pii> ranges;
     while (getline(cin, line)) {
         if (line.empty()) break;
-
         size_t dash_pos = line.find('-');
         if (dash_pos != string::npos) {
             string start_str = line.substr(0, dash_pos);
@@ -21,12 +20,8 @@ int main() {
             ranges.push_back({start_num, end_num});
         }
     }
-
     ull ans = 0;
-    sort(ranges.begin(), ranges.end(), [](const Range& a, const Range& b) {
-        if(a.first == b.first) return a.second < b.second;
-        return a.first < b.first;
-    });
+    sort(ranges.begin(), ranges.end());
     ull curStart = ranges.front().first;
     ull curEnd = ranges.front().second;
     for(auto& range : ranges) {
@@ -40,5 +35,8 @@ int main() {
         }
     }
     ans += curEnd - curStart + 1;
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
     cout << "ans:" << ans << endl;
+    cout << "runtime: " << duration.count() << " microseconds" << endl;
 }
