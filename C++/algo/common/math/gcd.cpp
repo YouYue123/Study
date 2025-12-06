583cef1b7ba54a43f5a2c3935dafab6a1aa22b3d
YouYue123 <youy0004123@gmail.com>
2025-12-06 20:35:57 +0800

    gcd

--- C++/algo/common/math/gcd.cpp
+++ C++/algo/common/math/gcd.cpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 // https://oi-wiki.org/math/number-theory/gcd/
-auto gcd = [&] (int a, int b) {
+auto gcd_robust = [](int a, int b) {
+    a = abs(a);
+    b = abs(b);
     while(b != 0) {
         int temp = b;
         b = a % b;
