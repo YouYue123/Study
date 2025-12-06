09f62b4c2517798d83006381cb0bf3bbb4fc39f0
YouYue123 <youy0004123@gmail.com>
2025-12-06 20:22:03 +0800

    gcd

--- C++/algo/common/math/gcd.cpp
+++ C++/algo/common/math/gcd.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int a = 48;
-    int b = 18;
-    int result = gcd(a, b);
-    cout << "GCD of " << a << " and " << b << " is: " << result << endl;
-    return 0;
-}
+auto gcd = [&] (int a, int b) {
+    while(b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+};
