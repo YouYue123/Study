1b2ae2697bf4246a29fc7394f912a84024ff456e
YouYue123 <youy0004123@gmail.com>
2025-12-07 13:38:37 +0800

    update

--- C++/algo/adventofcode/2025/7/7_2.cpp
+++ C++/algo/adventofcode/2025/7/7_2.cpp
@@ -5,10 +5,10 @@ using ull = unsigned long long;
 using pii = pair<ull, ull>;
 int main() {
     auto start = chrono::high_resolution_clock::now();
-    ull ans = 0;
+    ll ans = 0;
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    vector<vector<ull>> nums;
+    vector<vector<ll>> nums;
     string line = "";
     vector<string> g;
     while(getline(cin, line)) g.push_back(line);
@@ -17,16 +17,15 @@ int main() {
     for(int j = 0; j < m; j ++) {
         if(g[0][j] == 'S') sCol = j;
     }
-    vector memo(n, vector(m, ULLONG_MAX));
-    auto dfs = [&] (this auto&& dfs, int row, int col) -> ull {
-        if(row == n - 1) return 1ULL;
-        if(col < 0 || col >= m) return 0ULL;
-        if(memo[row][col] != ULLONG_MAX) return memo[row][col];
+    vector memo(n, vector(m, -1LL));
+    auto dfs = [&] (this auto&& dfs, int row, int col) -> ll {
+        if(row == n - 1) return 1;
+        if(col < 0 || col >= m) return 0;
+        if(memo[row][col] != -1) return memo[row][col];
         int nRow = row + 1;
-        long long ans = 0ULL;
+        long long ans = 0LL;
         if(g[nRow][col] == '^') {
-            ans += dfs(nRow, col - 1);
-            ans += dfs(nRow, col + 1);
+            ans = ans + dfs(nRow, col - 1) + dfs(nRow, col + 1);
         } else {
             ans = dfs(nRow, col);
         }
