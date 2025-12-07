56226844af01594cb9b6a2947eb93749d3a05b92
YouYue123 <youy0004123@gmail.com>
2025-12-07 11:38:01 +0800

    sieve

--- C++/algo/common/math/sieve.cpp
+++ C++/algo/common/math/sieve.cpp
@@ -1,14 +1,15 @@
+#include <bits/stdc++.h>
 const int MAX_N = 1e5;
-bool is_composite[MAX_N + 1]; 
-// is_composite[i] = true means i is composite, 
-// is_composite[i] = false means i is prime
-
+bool isPrime[MAX_N + 1];
 void sieve() {
-    is_composite[1] = true; // 1 is not a prime number
+    std::fill(isPrime, isPrime + MAX_N + 1, true);
+    // 1 is not prime
+    isPrime[1] = false;
     for (int p = 2; p * p <= MAX_N; ++p) {
-        if (!is_composite[p]) {
-            for (int multiple = p * p; multiple <= MAX_N; multiple += p) {
-                is_composite[multiple] = true; // Mark multiples of p as composite
+        if (isPrime[p]) {
+            for (int mult = p * p; mult <= MAX_N; mult += p) {
+                // Mark multiples of non-prime
+                isPrime[mult] = false; 
             }
         }
     }
