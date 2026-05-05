99d6e59283f914a5848f53882079a46f44f38d06
YouYue123 <youy0004123@gmail.com>
2026-05-05 23:23:16 +0800

    2217B

--- C++/algo/codeforces/2217/B.cpp
+++ C++/algo/codeforces/2217/B.cpp
@@ -6,6 +6,33 @@ ll constexpr INF = 0x3f3f3f3f3f3f3f3f;
 
 void solve()
 {
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for(int i = 0; i < n; i ++) cin >> a[i];
+    int p;
+    cin >> p;
+    vector b(n + 2, 0);
+    b[0] = 1;
+    b[n + 1] = 1;
+    for(int i = 0; i < n; i ++) {
+        if(a[i] == a[p - 1]) {
+            b[i + 1] = 1;
+        } else {
+            b[i + 1] = 0;
+        }
+    }
+    int left_cnt = 0, right_cnt = 0;
+    for(int i = 0; i <= n; i ++) {
+        if(b[i] != b[i + 1]) {
+            if(i < p) {
+                left_cnt += 1;
+            } else {
+                right_cnt += 1;
+            }
+        }
+    }
+    cout << max(left_cnt, right_cnt) << endl;
 }
 
 int main()
