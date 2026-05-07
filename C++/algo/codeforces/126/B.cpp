738356a46d1370c608034140600afe1dd5915e14
YouYue123 <youy0004123@gmail.com>
2026-05-07 16:11:49 +0800

    126b

--- C++/algo/codeforces/126/B.cpp
+++ C++/algo/codeforces/126/B.cpp
@@ -4,18 +4,43 @@ using ll = long long;
 using ull = unsigned long long;
 ll constexpr INF = 0x3f3f3f3f3f3f3f3f;
 
-void solve()
-{
-}
-
 int main()
 {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
-    int T;
-    cin >> T;
-    while (T--)
-    {
-        solve();
+    string s;
+    cin >> s;
+    int n = s.size();
+
+    auto get_lps = [&] () {
+        vector<int> lps(n, 0);
+        for(int i = 1, match = 0; i < n; i ++) {
+            while(match > 0 && s[i] != s[match]) match = lps[match - 1];
+            if(s[match] == s[i]) match += 1;
+            lps[i] = match;
+        }
+        return lps;
+    };
+
+    auto lps = get_lps();
+    int candidate = lps.back();
+    // bool ok = false;
+    int max_lps_middle = 0;
+    for(int i = 1; i < n - 1; i++) max_lps_middle = max(max_lps_middle, lps[i]);
+    while(candidate > max_lps_middle) {
+        candidate = lps[candidate - 1];
     }
+    // while(candidate > 0) {
+    //     for(int i = 1; i < n - 1; i ++) {
+    //         if(lps[i] == candidate) {
+    //             ok = true;
+    //             break;
+    //         }
+    //     }
+    //     if(!ok) candidate = lps[candidate - 1];
+    //     else break;
+    // }
+    // cout << endl;
+    if(candidate) cout << s.substr(0, candidate) << endl;
+    else cout << "Just a legend" << endl;
 }
--- C++/algo/common/strings/kmp.cpp
+++ C++/algo/common/strings/kmp.cpp
@@ -7,14 +7,13 @@ struct KMP
     // LSP -> longest proper prefix
     vector<int> get_lsp(const string &p)
     {
-        vector<int> pi(p.size());
+        vector<int> lsp(p.size());
         for (int i = 1, match = 0; i < p.size(); i++) {
-            char v = p[i];
-            while (match > 0 && p[match] != v)  match = pi[match - 1];
-            if (p[match] == v) match++;
-            pi[i] = match;
+            while (match > 0 && p[match] != p[i])  match = lsp[match - 1];
+            if (p[match] == p[i]) match++;
+            lsp[i] = match;
         }
-        return pi;
+        return lsp;
     }
     vector<int> kmp(const string &s, const string &p)
     {
