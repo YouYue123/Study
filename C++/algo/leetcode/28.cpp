369e3cb88a38f2d3ac1c009121e08696d2b9b549
YouYue123 <youy0004123@gmail.com>
2026-05-08 14:05:38 +0800

    lc 28

--- C++/algo/leetcode/28.cpp
+++ C++/algo/leetcode/28.cpp
@@ -1,30 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 28. Find the Index of the First Occurrence in a String [E]
-// LeetCode find-the-index-of-the-first-occurrence-in-a-string
-
 class Solution {
-public:
-    int strStr(string haystack, string needle) {
-        int n = haystack.size(), m = needle.size();
-        vector<int> next(m, 0);
-        for(int i = 1, j = 0; i < m ; i ++) {
-            while(j > 0 && needle[i] != needle[j]) {
-                j = next[j - 1];
+    public:
+        int strStr(string haystack, string needle) {
+            int m = haystack.size(), n = needle.size();
+            auto get_lps = [&] () {
+                vector<int> lps(n, 0);
+                for(int i = 1, match = 0; i < n; i ++) {
+                    while(match > 0 && needle[i] != needle[match]) match = lps[match - 1];
+                    if(needle[i] == needle[match]) match += 1;
+                    lps[i] = match;
+                }
+                return lps;
+            };
+            auto lps = get_lps();
+            for(int i = 0, j = 0; i < m; i ++) {
+                while(j > 0 && haystack[i] != needle[j]) j = lps[j - 1];
+                if(haystack[i] == needle[j]) j += 1;
+                // cout << j << endl;
+                if(j == n) {
+                    // cout << i << endl;
+                    return i - n + 1;
+                }
             }
-            if(needle[i] == needle[j]) j += 1;
-            next[i] = j;
-        }
-        for(int i = 0, j = 0; i < n; i ++) {
-            while(j > 0 && haystack[i] != needle[j] ) {
-                j = next[j - 1];
-            }
-            if(haystack[i] == needle[j]) j += 1;
-            if(j == m) {
-                return i - m + 1;
-            }
-        }
-        return -1;
+            return -1;
     }
 };
