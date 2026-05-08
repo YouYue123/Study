c72c844b6b9f5c11a78d22cfb5325299c13be893
YouYue123 <youy0004123@gmail.com>
2026-05-08 13:10:01 +0800

    lc 1735

--- C++/algo/leetcode/1735.cpp
+++ C++/algo/leetcode/1735.cpp
@@ -5,8 +5,8 @@ class Solution {
     static const int MOD = 1e9 + 7;
     // MAXN 需要覆盖 (最大指数 + 最大 n - 1)。
     // n 最大 10^4，k 最大 10^4 (其质因子指数总和很小)，15000 足够。
-    static const int MAXN = 15000; 
-    ll fact[MAXN], invFact[MAXN];
+    static const int MAXN = 1e5 + 1; 
+    ll fact[MAXN], invFact[MAXN], min_prime[MAXN];
     // 快速幂：用于计算费马小定理下的乘法逆元
     ll quick_pow(ll a, ll k) {
         ll ans = 1;
@@ -29,35 +29,40 @@ class Solution {
     }
 
     // O(1) 获取组合数
-    // C(n, r), choosing r items from total n
     ll nCr(int n, int r) {
         if (r < 0 || r > n) return 0;
         return fact[n] * invFact[r] % MOD * invFact[n - r] % MOD;
+    }
+    
+    // sieve 求min_prime 为质因数分解做准备
+    void sieve() {
+        if(min_prime[2] != 0) return;
+        for(int num = 2; num < MAXN; num ++) {
+            if(min_prime[num] == 0) {
+                for(int cur = num; cur < MAXN; cur+= num) {
+                    min_prime[cur] = num;
+                }
+            }
+        }
     }
 
 public:
     vector<int> waysToFillArray(vector<vector<int>>& queries) {
         precompute();
+        sieve();
         vector<int> ans;
         for (auto& q : queries) {
             int n = q[0], k = q[1];
             ll cur_ans = 1;
             // --- 对 k 进行质因数分解 ---
-            for (int i = 2; i * i <= k; i++) {
-                if (k % i == 0) {
-                    int cnt = 0;
-                    while (k % i == 0) { 
-                        cnt++;
-                        k /= i; 
-                    }
-                    // 将 cnt 个相同的质因子 i 放入 n 个不同的盒子
-                    cur_ans = cur_ans * nCr(cnt + n - 1, n - 1) % MOD;
+            while(k > 1) {
+                int p = min_prime[k];
+                int cnt = 0;
+                while(k % p == 0) {
+                    cnt += 1;
+                    k /= p;
                 }
-            }
-            // 如果最后还剩下一个质数（temp_k > 1）
-            if (k > 1) {
-                // 此时 cnt = 1，公式为 nCr(1 + n - 1, n - 1) = nCr(n, n - 1) = n
-                cur_ans = cur_ans * n % MOD;
+                cur_ans = cur_ans * nCr(cnt + n - 1, n - 1) % MOD;
             }
             ans.push_back(cur_ans);
         }
