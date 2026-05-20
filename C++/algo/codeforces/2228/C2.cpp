0143ff70eede967d1e79cefffd5bb07269f5d5ee
YouYue123 <youy0004123@gmail.com>
2026-05-20 17:49:54 +0800

    c2

--- C++/algo/codeforces/2228/C2.cpp
+++ C++/algo/codeforces/2228/C2.cpp
@@ -5,79 +5,84 @@ using ull = unsigned long long;
 using u128 = unsigned __int128;
 ll constexpr INF = 0x3f3f3f3f3f3f3f3f;
 
-u128 diff(u128 b, ll a) {
-    return abs((ll)b - a);
-}
-
 void solve()
 {
     ll a, n;
     cin >> a >> n;
-    vector<ll> d(n);
+    vector d(n, 0);
     for(int i = 0; i < n; i ++) cin >> d[i];
-    int d_min = d.front(), d_max = d.back();
-    if(d_min == 0 && d_max == 0) {
-        cout << a << endl;
-        return;
-    }
-    u128 b = 0, ans = -1;
-    string a_s = to_string(a);
-    int a_len = a_s.size();
+    // cout << d.size() << endl;
+    int d_min = d.front(), d_max = d[d.size() - 1];
+    // cout << d_min << " " << d_max << endl;
+    auto a_s = to_string(a);
+    auto a_len = a_s.size();
+    ll ans = INF;
+    // 1 digit less
     if(a_len > 1) {
+        ll cur = 0;
         for(int i = 0; i < a_len - 1; i ++) {
-            b = b * 10 + d_max;
+            cur = cur * 10 + d_max;
         }
-        ans = min(ans, diff(b, a));
+        // cout << "1 digit less: " << cur << endl;
+        ans = min(ans, abs(cur - a));
     }
-    
-    b = 0;
-    if(d_min == 0 && n > 1) {
-        b = d[1];
-    }
-    for(int i = 0; i < a_len; i ++) {
-        b = b* 10 + d_min;
-    }
-    ans = min(ans, diff(b, a));
 
-    auto dfs = [&] (this auto&& dfs, u128 cur_b, int len, bool is_larger, bool is_smaller) -> void {
+    // 1 digit more
+    if(d_min == 0) {
+        if(d.size() > 1) {
+            ll cur = d[1];
+            for(int i = 0; i < a_len; i ++) {
+                cur = cur * 10 + d_min;
+            }
+            ans = min(ans, abs(cur - a));
+        }
+    } else {
+        ll cur = 0;
+        for(int i = 0; i < a_len + 1; i ++) {
+            cur = cur * 10 + d_min;
+        }
+        ans = min(ans, abs(cur - a));
+    }
+
+    auto dfs = [&] (this auto&& dfs, ll cur, int len, bool is_larger, bool is_smaller) {
         if(len == a_len) {
-            ans = min(ans, diff(cur_b, a));
+            ans = min(ans, abs(cur - a)); 
             return;
         }
-        
+
         if(is_larger) {
             while(len < a_len) {
-                cur_b = cur_b * 10 + d_min;
+                cur = cur * 10 + d_min;
                 len += 1;
             }
-            ans = min(ans, diff(cur_b, a));
+            ans = min(ans, abs(cur - a)); 
             return;
         }
 
         if(is_smaller) {
             while(len < a_len) {
-                cur_b = cur_b * 10 + d_max;
+                cur = cur * 10 + d_max;
                 len += 1;
             }
-            ans = min(ans, diff(cur_b, a));
+            ans = min(ans, abs(cur - a)); 
             return;
         }
-        
-        for(int i = 0; i < n; i ++) {
-            auto nxt_b = cur_b * 10 + d[i];
-            if(a_s[len] - '0' == d[i]) {
-                dfs(nxt_b, len + 1, false, false);
-            } else if(a_s[len] - '0' < d[i]) {
-                dfs(nxt_b, len + 1, true, false);
+        int cur_a = a_s[len] - '0';
+        for(int cand : d) {
+            ll nxt = cur * 10 + cand;
+            if(cand == cur_a) {
+                dfs(nxt, len + 1, false, false);
+            } else if(cand < cur_a) {
+                dfs(nxt, len + 1, false, true);
             } else {
-                dfs(nxt_b, len + 1, false, true);
+                dfs(nxt, len + 1, true, false);
             }
         }
     };
 
     dfs(0, 0, false, false);
 
-    cout << (ll)ans << endl;
+    cout << ans << endl;
 }
 
 int main()
