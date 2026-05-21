6e47f6ac154ae631b3d283baa2dd92c17f435498
YouYue123 <youy0004123@gmail.com>
2026-05-21 22:21:14 +0800

    2230

--- C++/algo/codeforces/2230/D.cpp
+++ C++/algo/codeforces/2230/D.cpp
@@ -9,70 +9,41 @@ void solve()
 {
     ll n;
     cin >> n;
-    vector<ll> a(n), b(n);
-    for(int i = 0; i < n; i++) cin >> a[i];
-    for(int i = 0; i < n; i++) cin >> b[i];
+    vector a(n, 0LL);
+    for(int i = 0; i < n; i ++) cin >> a[i];
+    vector b(n, 0LL);
+    for(int i = 0; i < n; i ++) cin >> b[i];
+    
+    // pa[i] stands for the earlist day for episode i in the future
+    // pa[i] == n means it never happens
+    vector<int> pa(n + 2, n), pb (n + 2, n);
+    // dp[i] stands for if at day i both watched the same episode, the furthest day it can reach
+    vector<int> dp(n, n - 1);
 
-    vector<vector<ll>> match_pos(n + 2);
-    vector<vector<ll>> bad_pos(n + 2);
+    ll ans = 0;
+    for(int L = n - 1; L >= 0; L--) {
+        int episode_a = a[L], episode_b = b[L];
+        pa[episode_a] = L;
+        pb[episode_b] = L;
 
-    for(int i = 0; i < n; i++) {
-        if (a[i] == b[i]) {
-            match_pos[a[i]].push_back(i);
-        } else {
-            bad_pos[a[i]].push_back(i);
-            bad_pos[b[i]].push_back(i);
-        }
-    }
-
-    vector<int> dp(n + 1, n);
-
-    for(int i = n - 1; i >= 0; i--) {
-        if (a[i] == b[i]) {
-            int v = a[i] + 1;
-            int nxt_m = n;
-            auto it_m = lower_bound(match_pos[v].begin(), match_pos[v].end(), i + 1);
-            if (it_m != match_pos[v].end()) nxt_m = *it_m;
-
-            int nxt_b = n;
-            auto it_b = lower_bound(bad_pos[v].begin(), bad_pos[v].end(), i + 1);
-            if (it_b != bad_pos[v].end()) nxt_b = *it_b;
-
-            if (nxt_b < nxt_m) {
-                dp[i] = nxt_b;
-            } else {
-                if (nxt_m == n) {
-                    dp[i] = n;
+        if(episode_a == episode_b) {
+            int nxt_episode = episode_a + 1;
+            if(nxt_episode <= n + 1) {
+                if(pa[nxt_episode] == pb[nxt_episode]) {
+                    dp[L] = (pa[nxt_episode] == n) ? n - 1 : dp[pa[nxt_episode]];
                 } else {
-                    dp[i] = dp[nxt_m];
+                    dp[L] = min(pa[nxt_episode], pb[nxt_episode]) - 1;
                 }
             }
         }
-    }
-
-    ll ans = 0;
-    for(int L = 0; L < n; L++) {
-        int nxt_m = n;
-        auto it_m = lower_bound(match_pos[1].begin(), match_pos[1].end(), L);
-        if (it_m != match_pos[1].end()) nxt_m = *it_m;
-
-        int nxt_b = n;
-        auto it_b = lower_bound(bad_pos[1].begin(), bad_pos[1].end(), L);
-        if (it_b != bad_pos[1].end()) nxt_b = *it_b;
-
-        int F_L = n;
-        if (nxt_b < nxt_m) {
-            F_L = nxt_b;
+        int R = 0;
+        if(pa[1] == pb[1]) {
+            R = (pa[1] == n) ? n - 1 : dp[pa[1]];
         } else {
-            if (nxt_m == n) {
-                F_L = n;
-            } else {
-                F_L = dp[nxt_m];
-            }
+            R = min(pa[1], pb[1]) - 1;
         }
-        ans += (F_L - L);
+        ans += (R - L + 1);
     }
-
     cout << ans << endl;
 }
 
