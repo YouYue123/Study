a4754134f0ead3376e717e09bbebb3f338b9eb89
YouYue123 <youy0004123@gmail.com>
2026-05-24 14:31:32 +0800

    276C

--- C++/algo/codeforces/276/C.cpp
+++ C++/algo/codeforces/276/C.cpp
@@ -7,6 +7,32 @@ ll constexpr INF = 0x3f3f3f3f3f3f3f3f;
 
 void solve()
 {
+    int n, q;
+    cin >> n >> q;
+    vector a(n, 0LL);
+    for(int i = 0; i < n; i ++) cin >> a[i];
+    vector diff(n + 1, 0LL);
+    for(int i = 0; i < q; i ++) {
+        int l, r;
+        cin >> l >> r;
+        l -= 1;
+        r -= 1;
+        diff[l] += 1;
+        diff[r + 1] -= 1;
+    }
+    vector freq(n, 0LL);
+    int cur = 0;
+    for(int i = 0 ; i < n; i ++) {
+        cur += diff[i];
+        freq[i] = cur;
+    }
+    sort(freq.begin(), freq.end(), greater<>());
+    sort(a.begin(), a.end(), greater<>());
+    ll ans = 0;
+    for(int i = 0; i < n; i ++) {
+        ans += freq[i] * a[i];
+    }
+    cout << ans << endl;
 }
 
 int main()
