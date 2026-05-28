303fb7be40def5aad895cf87785119bd5acc0028
YouYue123 <youy0004123@gmail.com>
2026-05-28 17:27:04 +0800

    462

---
+++ C++/algo/leetcode/462.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        int minMoves2(vector<int>& nums) {
+            int n = nums.size();
+            sort(nums.begin(), nums.end());
+            int median = nums[n / 2];
+            int ans = 0;
+            for(int num : nums) {
+                ans += abs(num - median);
+            }
+            return ans;
+        }
+    };
