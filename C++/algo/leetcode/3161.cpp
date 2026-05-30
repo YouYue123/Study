0a068c199de5e51c61e149a9b4e25bb9168a933c
YouYue123 <youy0004123@gmail.com>
2026-05-30 11:41:31 +0800

    3161

--- C++/algo/leetcode/3161.cpp
+++ C++/algo/leetcode/3161.cpp
@@ -3,26 +3,24 @@ using namespace std;
 
 struct BIT {
     vector<int> tree;
-    BIT(int N) {
-        tree.resize(N + 1, 0);
+    BIT (int N) {
+        tree.resize(N, 0);
     }
-
-    void update(int idx, const vector<int>& a) {
-        while (idx < tree.size()) {
+    void update(int idx, vector<int>& a) {
+        while(idx < tree.size()) {
             tree[idx] = a[idx];
-            int managed_len = idx & -idx; 
-            for (int len = 1; len < managed_len; len <<= 1) {
-                tree[idx] = max(tree[idx], tree[idx - len]); 
+            for(int i = 1; i < (idx & -idx); i *= 2) {
+                tree[idx] = max(tree[idx], tree[idx - i]);
             }
-            idx += managed_len;
+            idx += idx & -idx;
         }
     }
 
-    int query(int idx) {
+    int query (int idx) {
         int ans = 0;
-        while (idx > 0) {
+        while(idx > 0) {
             ans = max(ans, tree[idx]);
-            idx -= idx & (-idx);
+            idx -= idx & -idx;
         }
         return ans;
     }
@@ -31,57 +29,33 @@ struct BIT {
 class Solution {
 public:
     vector<bool> getResults(vector<vector<int>>& queries) {
-        // 动态寻找 queries 中出现的最大坐标，用来动态开 BIT 空间，防止硬编码越界
-        int max_x = 0;
-        for (const auto& q : queries) {
-            max_x = max(max_x, q[1]);
-        }
-        
-        vector<bool> ans;
         set<int> insertions;
-        insertions.insert(0); // 初始左边界
-        
-        // 空间大小根据最大坐标 max_x 来开即可
-        vector<int> a(max_x + 1, 0);
-        BIT bit(max_x);
-
-        for (auto& q : queries) {
-            if (q[0] == 1) {
-                int x = q[1];
-                auto it = insertions.lower_bound(x);
-                if (it != insertions.end()) {
-                    // 情况 A：x 插入到了两个已有分割点中间
-                    int right_nbr = *it;
-                    int left_nbr = *prev(it);
-                    
-                    // 右邻居的左边界变成了 x，间距缩水
-                    a[right_nbr] = right_nbr - x;
-                    bit.update(right_nbr, a);
-                    
-                    // x 自己的左边界是原先的 left_nbr
-                    a[x] = x - left_nbr;
-                    bit.update(x, a);
-                } else {
-                    // 情况 B：x 插入到了当前所有分割点的最右边
-                    int left_nbr = *prev(it);
-                    a[x] = x - left_nbr;
-                    bit.update(x, a);
+        insertions.insert(0);
+        vector<int> a(1e5 + 1);
+        BIT bit(1e5 + 1);
+        vector<bool> ans;
+        for(auto& q : queries) {
+            int type = q[0], x = q[1];
+            auto it = insertions.upper_bound(x);
+            auto left = *prev(it);
+            if(type == 1) {
+                if(it != insertions.end()) {
+                    auto right = *it;
+                    a[right] = right - x;
+                    bit.update(right, a);
                 }
+                a[x] = x - left;
+                bit.update(x, a);
                 insertions.insert(x);
             } else {
-                int x = q[1], sz = q[2];                
-                auto it = insertions.upper_bound(x); 
-                int left_closest = *prev(it);
-                // 【情况①】：检查最右侧残余区间 [left_closest, x] 够不够大
-                if (x - left_closest >= sz) {
+                int sz = q[2];
+                if(x - left >= sz) {
                     ans.push_back(true);
                     continue;
                 }
-                // 【情况②】：最右侧不够，去 BIT 里面查 [0, left_closest] 内部的最大间距
-                ans.push_back(bit.query(left_closest) >= sz);
-
+                ans.push_back(bit.query(left) >= sz);
             }
-        }
-        return ans;   
+        } 
+        return ans;     
     }
 };
