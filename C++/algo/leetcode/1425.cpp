a67e68d101f1fcd860511ef1ef5b0800b11d7960
YouYue123 <youy0004123@gmail.com>
2026-06-12 19:53:25 +0800

    update

--- C++/algo/leetcode/1425.cpp
+++ C++/algo/leetcode/1425.cpp
@@ -4,37 +4,25 @@ using namespace std;
 // 1425. Constrained Subsequence Sum [H]
 // LeetCode constrained-subsequence-sum
 
-class MonoQueue {
-    deque<int> q;
-    deque<int> maxQ;
-
-public:
-    void push(int i) {
-        while (!maxQ.empty() && maxQ.back() < i) maxQ.pop_back();
-        maxQ.push_back(i);
-        q.push_back(i);
-    }
-    int max() { return maxQ.front(); }
-    void pop() {
-        if (q.front() == maxQ.front()) maxQ.pop_front();
-        q.pop_front();
-    }
-    int size() { return q.size(); }
-};
-
+int constexpr INF = 0x3f3f3f3f;
 class Solution {
 public:
     int constrainedSubsetSum(vector<int>& nums, int k) {
-        MonoQueue mq;
-        vector<int> dp(nums.size());
-        dp[0] = nums[0];
-        mq.push(dp[0]);
-        for (int i = 1; i < nums.size(); i++) {
-            dp[i] = max(nums[i], mq.max() + nums[i]);
-            if(mq.size() == k) mq.pop();
-            mq.push(dp[i]);
+        int n = nums.size();
+        vector dp(n, 0);
+        deque<long long> q;
+        int ans = -INF;
+        for(int i = 0; i < n; i ++) {
+            dp[i] = q.empty() ? nums[i] : max(dp[q.front()] + nums[i], nums[i]);
+            while(!q.empty() && dp[q.back()] <= dp[i]) {
+                q.pop_back();
+            }
+            q.push_back(i);
+            if(i - q.front() == k) {
+                q.pop_front();
+            }
+            ans = max(ans, dp[i]);
         }
-
-        return *max_element(dp.begin(), dp.end());
+        return ans;
     }
 };
