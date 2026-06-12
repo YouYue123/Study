e83b240257c2a5a22286652d97347a239aee2f03
YouYue123 <youy0004123@gmail.com>
2026-06-12 19:14:11 +0800

    862.cpp

--- C++/algo/leetcode/862.cpp
+++ C++/algo/leetcode/862.cpp
@@ -4,45 +4,58 @@ using namespace std;
 // 862. Shortest Subarray with Sum at Least K [H]
 // LeetCode shortest-subarray-with-sum-at-least-k
 
-class MonoQueue {
+int constexpr INF = 0x3f3f3f3f;
+template<typename F>
+concept Predicate = requires(F f, int a, int b) {
+    { f(a, b) } -> convertible_to<bool>;
+};
+template<Predicate PushCmp, Predicate PopCmp>
+struct MonoQueue {
+    PushCmp push_cmp;
+    PopCmp pop_cmp;
     deque<int> q;
-    deque<int> minQ;
-public:
-    void push(int i) {
-        q.push_back(i);
-        while(!minQ.empty() && minQ.back() > i) minQ.pop_back();
-        minQ.push_back(i);
+
+    void push(int idx) {
+        while(!q.empty() && push_cmp(q.back(), idx)) q.pop_back();
+        q.push_back(idx);
     }
-    int min() { return minQ.front(); }
-    void pop() {
-        if(q.front() == minQ.front()) minQ.pop_front();
-        q.pop_front();
+
+    void pop(int idx) {
+        if(!q.empty() && pop_cmp(q.front(), idx)) {
+            q.pop_front();
+        }
     }
-    bool empty() { return q.empty(); }
+
+    int front() {
+        return q.front();
+    }
+    bool empty() {
+        return q.empty();
+    }
 };
 class Solution {
 public:
     int shortestSubarray(vector<int>& nums, int k) {
-        vector<long> preSum(nums.size() + 1, 0);
-        for(int i = 1; i <= nums.size(); i ++) {
-            preSum[i] = preSum[i - 1] + nums[i - 1];
-        }
-        int left = 0, right = 0;
-        MonoQueue q;
-        int ans = INT_MAX;
-        while(right < preSum.size()) {
-            q.push(preSum[right]);
-            while(!q.empty() && preSum[right] - q.min() >= k) {
-                if(preSum[right] - preSum[left] >= k) {
-                    // cout << left << " " << right << endl;
-                    // cout << preSum[right] - preSum[left] << endl;
-                    ans = min(ans, right - left);
-                }
-                q.pop();
-                left += 1;
+        int n = nums.size();
+        vector presum(n + 1, 0LL);
+        for(int i = 0; i < n; i ++) presum[i + 1] = presum[i] + nums[i];
+        auto push_cmp = [&] (int back_idx, int cur_idx) {
+            return presum[back_idx] >= presum[cur_idx];
+        };
+        auto pop_cmp = [&] (int front_idx, int cur_idx) {
+            return true;
+        };
+        auto mq = MonoQueue<decltype(push_cmp), decltype(pop_cmp)>(push_cmp, pop_cmp);
+        int right = 0;
+        int ans = INF;
+        while(right < n + 1) {
+            mq.push(right);
+            while(!mq.empty() && presum[right] - presum[mq.front()] >= k) {
+                ans = min(ans, right - mq.front());
+                mq.pop(right);
             }
             right += 1;
         }
-        return ans == INT_MAX ? -1 : ans;
+        return ans == INF ? -1 : ans;
     }
 };
